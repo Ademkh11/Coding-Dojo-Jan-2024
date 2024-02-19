c99cf8df702c55fd850b453185c0400f6a9db7c6
Ademkh11 <156675564+Ademkh11@users.noreply.github.com>
2024-02-19 17:31:03 +0100

    Add files via upload

---
+++ Python/Week 1/Day 1/Practice/hello_world.py
@@ -0,0 +1,11 @@
+# print("Hello World")
+name = "Adem"
+print("Hello", name + " !")
+num = 7
+# print("Hello" , num , " !")
+print("Hello " + str(num) + " !")
+food_one = "Pizza"
+food_two = "spaghetti"
+# print("i love to eat " + str (food_one) + " and " + str(food_two))
+print(f"i love to eat {food_one} and {food_two}")
+
