73916be1bdd57a46be5787fa0b2dea1ec2bd3952
Ademkh11 <156675564+Ademkh11@users.noreply.github.com>
2024-02-28 16:43:17 +0100

    Add files via upload

---
+++ Python/Week 2/Day 2/Practice/users_workbench.sql
@@ -0,0 +1,13 @@
+SELECT * FROM users_schema.users;
+SELECT * FROM users;
+SELECT * FROM users WHERE email = 'Joe_Doe@.com';
+SELECT * FROM users WHERE id =3;
+UPDATE users
+SET last_name ='pancakes'
+WHERE id=3; 
+DELETE FROM users WHERE id =2;
+SELECT first_name FROM users;
+
+
+
+
